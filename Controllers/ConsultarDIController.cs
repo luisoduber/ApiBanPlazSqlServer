7e7045d592cd5f6672a91a61c933cfec2f932735
LUIS ODUBER <luisoduber12@gmail.com>
2026-02-02 16:04:12 -0400

    act. consultarDI
    act. consultarDI

--- Controllers/ConsultarDIController.cs
+++ Controllers/ConsultarDIController.cs
@@ -1,7 +1,9 @@
-﻿using ApiBanPlaz.models.Entities;
-using ApiBanPlaz.models.ConsultarDl;
+﻿using ApiBanPlaz.models.ConsultarDl;
+using ApiBanPlaz.models.Entities;
+using ApiBanPlaz.Servicios.ConsultarDl;
 using ApiBanPlaz.Servicios.General;
 using ApiBanPlaz.Servicios.TokenDl;
+using Azure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
@@ -11,7 +13,6 @@ using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
 using System.Text;
-using ApiBanPlaz.Servicios.ConsultarDl;
 
 [ApiController]
 [Route("v1/cce/debinm")]
@@ -24,6 +25,7 @@ public class ConsultarDIController : ControllerBase
     private readonly IConfiguration _config;
     string urlBan = "";
     int idConsultarDI = 0;
+    ConsultarDlReq _ConsultarDlReq = new ConsultarDlReq();
 
     ConsultarDlResp _ConsultarDIResp = new ConsultarDlResp();
     ConsultarDI _ConsultarDI = new ConsultarDI();
@@ -36,43 +38,48 @@ public class ConsultarDIController : ControllerBase
         urlBan = _config["urlBan"].ToString();
         _ConsultarDIService = ConsultarDIService;
     }
+   
+    [HttpGet("consultarDI/{id}")]
+    public async Task<IActionResult> ConsultarDI(string id,
+        [FromQuery] string cuenta_cobrador,
+        [FromQuery] string endtoend,
+        [FromQuery] string referencia_c,
+        [FromQuery] decimal monto,
+        [FromQuery] int canal)
 
-    [HttpPost("ConsultarDl")]
-    public async Task<IActionResult> ConsultarDI()
     {
-        // 1. Leer el body como string "crudo"
-        string reqConsultarDI = "";
-        using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
-        {
-            reqConsultarDI = await reader.ReadToEndAsync();
-        }
-
-        var _ReqConsultarDI = JsonConvert.DeserializeObject<ConsultarDlReq>(reqConsultarDI);
-        if (_ReqConsultarDI == null) return BadRequest("Cuerpo de petición inválido.");
+        string queryStringDI= "";
+        
 
         string nonce = await _nonceService.ObtNonce();
         var cred = await _credApiRsService.ObtCredApi();
         if (cred == null) return NotFound();
 
-        string path = "v1/cce/debinm/ConsultarDI";
+        string path = "v1/cce/debinm/consultarDI/";
         string apiSignature = ApiSignatureGen.Generar(
             path,
             nonce,
-            reqConsultarDI,
             cred.apiKeySecret
         );
 
-        _ConsultarDIResp = await SolConsultarDI(reqConsultarDI, cred.ApiKey,apiSignature, nonce);
+        queryStringDI= 
+                       "?cuenta_cobrador=" + cuenta_cobrador +
+                       "&endtoend=" + endtoend +
+                       "&referencia_c=" + referencia_c +
+                       "&monto=" + monto.ToString("F2") +
+                       "&canal=" + canal.ToString();
+
+        _ConsultarDIResp = await SolConsultarDI(id, queryStringDI, cred.ApiKey,apiSignature, nonce);
         //return Ok(new { nonce,cred.ApiKey,cred.apiKeySecret,apiSignature});
 
         _ConsultarDI.IdConsultarDI= await _ConsultarDIService.GrdConsultarDIAsync(
-            _ReqConsultarDI.Id,
-            _ReqConsultarDI.cuenta_cobrador,
-            _ReqConsultarDI.endtoend,
-            _ReqConsultarDI.referencia_c,
-            _ReqConsultarDI.Monto,
-            _ReqConsultarDI.canal,
-            reqConsultarDI
+             _ConsultarDlReq.Id,
+             _ConsultarDlReq.cuenta_cobrador,
+             _ConsultarDlReq.endtoend,
+             _ConsultarDlReq.referencia_c,
+             _ConsultarDlReq.Monto,
+             _ConsultarDlReq.canal,
+            queryStringDI
             );
 
         string jsonConsultarDIResp = JsonConvert.SerializeObject(_ConsultarDIResp);
@@ -96,11 +103,11 @@ public class ConsultarDIController : ControllerBase
         });
     }
 
-    public async Task<ConsultarDlResp> SolConsultarDI(string prmJson, string prmApiKey, 
-                                         string prmApiSignature, string prmNonce)
+    public async Task<ConsultarDlResp> SolConsultarDI(string prmId, string prmQryString, 
+                                                     string prmApiKey, string prmApiSignature, 
+                                                     string prmNonce)
     {
         string rsDat = "";
-
         string codigoRespuesta = "";
         string descripcionCliente = "";
         string descripcionSistema = "";
@@ -108,7 +115,6 @@ public class ConsultarDIController : ControllerBase
 
         using (var client = new HttpClient())
         {
-            var content = new StringContent(prmJson, Encoding.UTF8, "application/json");
             client.BaseAddress = new Uri(urlBan);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("api-key", prmApiKey);
@@ -116,8 +122,16 @@ public class ConsultarDIController : ControllerBase
             client.DefaultRequestHeaders.Add("nonce", prmNonce);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            using (var Res = await client.PostAsync("v1/cce/debinm/ConsultarDI", content))
+
+            Debug.WriteLine($"prmApiKey: {prmApiKey}");
+            Debug.WriteLine($"prmApiSignature: {prmApiSignature}");
+            Debug.WriteLine($"prmNonce: {prmNonce}");
+
+            Debug.WriteLine($"{urlBan}v1/cce/debinm/ConsultarDI/{prmId}{prmQryString}");
+            using (var Res = await client.GetAsync($"v1/cce/debinm/ConsultarDI/{prmId}{prmQryString}"))
             {
+
+                Debug.WriteLine("respuesta: "+Res.IsSuccessStatusCode);
                 if (Res.Headers.TryGetValues("codigoRespuesta", out var values)) { codigoRespuesta= values.FirstOrDefault(); }
                 if (Res.Headers.TryGetValues("descripcionCliente", out var values1)) { descripcionCliente = values1.FirstOrDefault(); }
                 if (Res.Headers.TryGetValues("descripcionSistema", out var values2)) { descripcionSistema = values2.FirstOrDefault(); }
@@ -141,12 +155,10 @@ public class ConsultarDIController : ControllerBase
         return _ConsultarDIResp;
     }
 
-
     public static class ApiKeyGen
     {
         public static string GenApiKey()
         {
-            // 16 bytes = 32 caracteres hex
             byte[] bytes = new byte[16];
 
             using (var rng = RandomNumberGenerator.Create())
@@ -166,29 +178,21 @@ public class ConsultarDIController : ControllerBase
             using var rng = RandomNumberGenerator.Create();
             rng.GetBytes(buffer);
 
-            return Convert.ToHexString(buffer).ToLower(); //  32
+            return Convert.ToHexString(buffer).ToLower();
         }
     }
 
 public static class ApiSignatureGen
 {
-    public static string Generar(string path, string nonce, string body, string secret)
+    public static string Generar(string path, string nonce, string secret)
     {
-        // 1. Recrear la cadena de firma exactamente como en el JS de Postman:
-        // let signature = `/${apiPath}${nonce}${body}`;
-        // Asegúrate de que 'path' no tenga la '/' inicial al pasarlo, o ajusta aquí:
-        string signatureRaw = $"/{path}{nonce}{body}";
-
-        // 2. Convertir a bytes usando UTF-8
+        string signatureRaw = $"/{path}{nonce}";
         byte[] keyBytes = Encoding.UTF8.GetBytes(secret);
         byte[] messageBytes = Encoding.UTF8.GetBytes(signatureRaw);
 
-        // 3. Calcular HMAC SHA384
         using (var hmac = new HMACSHA384(keyBytes))
         {
             byte[] hashBytes = hmac.ComputeHash(messageBytes);
-
-            // 4. Convertir a Hexadecimal (minúsculas como hace CryptoJS por defecto)
             return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
         }
     }
--- obj/Debug/net9.0/ApiBanPlaz.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiBanPlaz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41e3a070d4814382cce09e3a78d136a9b4d3f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af580c38cdc3828d573242a9d0ae2be14bbda126")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
