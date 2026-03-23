77c05a54ca529cd3791e354cb086b86bb48a3b73
LUIS ODUBER <luisoduber12@gmail.com>
2026-03-23 16:51:34 -0400

    act
    act

--- models/CompPm/CompPmReq.cs
+++ models/CompPm/CompPmReq.cs
@@ -5,25 +5,13 @@ namespace ApiBanPlaz.models.CompPm
     public class CompPmReq
     {
         public string id { get; set; }
-
-        [StringLength(2)]
         public string canal { get; set; }
-
         public int acc { get; set; }
-
         public DateTime fi { get; set; }
         public DateTime ff { get; set; }
-
-        [StringLength(11)]
         public string tlf { get; set; }
-
-        [StringLength(11)]
         public string tlfa { get; set; }
-
-        [StringLength(8)]
         public string horaIni { get; set; }
-        
-        [StringLength(8)]
         public string horaFin { get; set; }
     }
 }
--- obj/Debug/net9.0/ApiBanPlaz.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiBanPlaz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc0f168fa0c845ac957440e5d47dc08dbf371b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04f0ce9a6ca1cace874445f3cd00269b436f8f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiBanPlaz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
